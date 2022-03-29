71b900e5efcbe96dfe77f830f636c896f00c91d3
degen <robovault1234@gmail.com>
2022-03-29 16:08:59 +1100

    updated migration tests

--- tests/test_offsetprice.py
+++ tests/test_offsetprice.py
@@ -514,3 +514,122 @@ def test_price_offset_checks_B(
         chain.sleep(5)
         chain.mine(5)
         vault.withdraw(withdrawAmt, user, 1000, {'from' : user})  
+
+
+def test_migration_offsetA(
+    chain,
+    tokens,
+    vaults,
+    strategies,
+    amounts,
+    strategy_contract,
+    jointLP_contract,
+    scTokens,
+    conf,
+    strategist,
+    gov,
+    user,
+    RELATIVE_APPROX,
+    whales,
+    Contract
+):
+    # Deposit to the vault and harvest
+    for i in range(len(tokens)) : 
+        token = tokens[i]
+        vault = vaults[i]
+        strategy = strategies[i]
+        amount = amounts[i]
+        token.approve(vault.address, amount, {"from": user})
+        vault.deposit(amount, {"from": user})
+        strategy.harvest()
+        assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
+
+    farmToken = conf['harvest_tokens'][0]
+    newJointLP = jointLP_contract.deploy(conf['LP'], conf['farm'] , conf['farmPID'], conf['router'], farmToken, {'from' : gov})
+
+    newStrategies = []
+
+    for i in range(len(tokens)) : 
+        token = tokens[i]
+        scToken = scTokens[i]
+        vault = vaults[i]
+        newStrategy = strategy_contract.deploy(vault, newJointLP, scToken, conf['comptroller'], conf['router'], conf['compToken'], {"from": strategist} )        
+        newStrategies = newStrategies + [newStrategy]
+
+    newJointLP.initaliseStrategies(newStrategies, {"from": gov})
+
+    tokenIndex = 0
+    swapPct = 0.02
+
+    offSetDebtRatio(gov, whales, tokens, conf, Contract, tokenIndex, swapPct)
+
+    chain.sleep(1)
+    chain.mine(1)
+
+    for i in range(len(tokens)) : 
+        strategy = strategies[i]
+        newStrategy = newStrategies[i]
+        vault = vaults[i]
+        vault.migrateStrategy(strategy, newStrategy, {"from": gov})
+        amount = amounts[i]
+        assert (pytest.approx(newStrategy.estimatedTotalAssets(), rel=RELATIVE_APPROX)  == amount )
+
+def test_migration_offsetB(
+    chain,
+    tokens,
+    vaults,
+    strategies,
+    amounts,
+    strategy_contract,
+    jointLP_contract,
+    scTokens,
+    conf,
+    strategist,
+    gov,
+    user,
+    RELATIVE_APPROX,
+    whales,
+    Contract
+):
+    # Deposit to the vault and harvest
+    for i in range(len(tokens)) : 
+        token = tokens[i]
+        vault = vaults[i]
+        strategy = strategies[i]
+        amount = amounts[i]
+        token.approve(vault.address, amount, {"from": user})
+        vault.deposit(amount, {"from": user})
+        strategy.harvest()
+        assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
+
+    farmToken = conf['harvest_tokens'][0]
+    newJointLP = jointLP_contract.deploy(conf['LP'], conf['farm'] , conf['farmPID'], conf['router'], farmToken, {'from' : gov})
+
+    newStrategies = []
+
+    for i in range(len(tokens)) : 
+        token = tokens[i]
+        scToken = scTokens[i]
+        vault = vaults[i]
+        newStrategy = strategy_contract.deploy(vault, newJointLP, scToken, conf['comptroller'], conf['router'], conf['compToken'], {"from": strategist} )        
+        newStrategies = newStrategies + [newStrategy]
+
+    newJointLP.initaliseStrategies(newStrategies, {"from": gov})
+
+    tokenIndex = 1
+    swapPct = 0.02
+
+    offSetDebtRatio(gov, whales, tokens, conf, Contract, tokenIndex, swapPct)
+
+    chain.sleep(1)
+    chain.mine(1)
+
+    for i in range(len(tokens)) : 
+        strategy = strategies[i]
+        newStrategy = newStrategies[i]
+        vault = vaults[i]
+        vault.migrateStrategy(strategy, newStrategy, {"from": gov})
+        amount = amounts[i]
+        assert (pytest.approx(newStrategy.estimatedTotalAssets(), rel=RELATIVE_APPROX)  == amount )
+
+
